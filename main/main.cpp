988107ded05b81797c941e22d37388dfb336108c
Mathias-Mittermair <mathias.mittermair@gmx.at>
2025-09-26 21:39:03 +0200

    Now the online Settings set the led strip color

--- main/main.cpp
+++ main/main.cpp
@@ -11,6 +11,8 @@ static const char *TAG = "Main";
 
 extern "C" void app_main(void)
 {
+    LEDStrip strip(17, 80, false);
+
     // === STEP 1: Initialize NVS ===
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NEW_VERSION_FOUND || ret == ESP_ERR_NVS_INVALID_STATE)
@@ -54,29 +56,13 @@ extern "C" void app_main(void)
                      settings.red, settings.green, settings.blue,
                      settings.brightness, settings.duration_minutes);
 
-            // Your LED logic here:
-            // LEDStrip strip(17, 80, false);
-            // strip.setAll(settings.red, settings.green, settings.blue);
-            // etc.
+            for (int i = 0; i < 80; i++)
+            {
+                strip.setPixel(i, settings.red, settings.green, settings.blue);
+            }
+            strip.refresh();
         }
 
         vTaskDelay(pdMS_TO_TICKS(5000)); // Update every 5 sec
     }
-
-    // LEDStrip strip(17, 80, false);
-
-    // while (true) {
-    //     for (int i = 0; i < 80; i++) {
-    //         strip.setPixel(i, 20, 0, 0);
-    //     }
-    //     strip.refresh();
-    //     ESP_LOGI(TAG, "LEDs ON");
-
-    //     vTaskDelay(pdMS_TO_TICKS(1000));
-
-    //     strip.clear();
-    //     ESP_LOGI(TAG, "LEDs OFF");
-
-    //     vTaskDelay(pdMS_TO_TICKS(1000));
-    // }
 }
